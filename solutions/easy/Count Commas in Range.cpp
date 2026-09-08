9bf891f5ecb811a618afdddcda650b7b55860b8d
vishalmishraxr-mp <vishalmishraxr@gmail.com>
2026-09-08 13:47:26 +0530

    Add solution for Count Commas in Range

---
+++ solutions/easy/Count Commas in Range.cpp
@@ -0,0 +1,12 @@
+// Title: Count Commas in Range
+            // Difficulty: Easy
+            // Language: C++
+            // Link: https://leetcode.com/problems/count-commas-in-range/
+
+class Solution {
+public:
+    int countCommas(int n) {
+    }
+        if(n<=999) return 0;
+        else return n-999;
+};
