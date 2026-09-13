829ab85543605999ad850ff5f9ec74d3e67de368
vishalmishraxr-mp <vishalmishraxr@gmail.com>
2026-09-13 19:36:27 +0530

    Add solution for Unique 3-Digit Even Numbers

--- solutions/easy/Unique 3-Digit Even Numbers.cpp
+++ solutions/easy/Unique 3-Digit Even Numbers.cpp
@@ -5,12 +5,16 @@
 
                     if (arr[k] % 2 != 0) continue;
 
-                    int num = arr[i] * 100 + arr[j] * 10 + arr
-                    [k];
-                    st.insert(num);
-                }
-            }
-        }
-        return st.size();
-    }
-};
+                    if (k == i || k == j) continue;
+                for (int k = 0; k < n; k++) {
+                if (j == i) continue;
+
+            for (int j = 0; j < n; j++) {
+            if (arr[i] == 0) continue; 
+        for (int i = 0; i < n; i++) {
+
+        int n = arr.size();
+        set<int> st;
+public:
+    int totalNumbers(vector<int>& arr) {
+class Solution {
