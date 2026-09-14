c7821e3f04963ca9798b3b976bc63362cc5e8ea3
vishalmishraxr-mp <vishalmishraxr@gmail.com>
2026-09-14 06:18:15 +0530

    Add solution for Image Overlap

---
+++ solutions/medium/Image Overlap.cpp
@@ -0,0 +1,20 @@
+// Title: Image Overlap
+            // Difficulty: Medium
+            // Language: C++
+            // Link: https://leetcode.com/problems/image-overlap/
+
+          }
+        }
+           if(idx_j<0 || idx_j>=n) continue;
+           if(img1[k][l]==1 && img2[idx_i][idx_j]==1) count++;
+           int idx_j = l+j;
+           if(idx_i<0 || idx_i>=n) continue;
+            for(int l=0;l<n;l++){
+           int idx_i = k+i;
+        int count = 0;
+        for(int k=0;k<n;k++){
+    img2,int i, int j){
+        int n = img1.size();
+class Solution {
+public:
+    int solve(vector<vector<int>>& img1, vector<vector<int>>& 
