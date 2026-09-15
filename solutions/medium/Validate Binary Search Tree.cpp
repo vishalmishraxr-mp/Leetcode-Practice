3a1937a875492373325b830745b803ebd26a9353
vishalmishraxr-mp <vishalmishraxr@gmail.com>
2026-09-15 23:01:44 +0530

    Add solution for Validate Binary Search Tree

---
+++ solutions/medium/Validate Binary Search Tree.cpp
@@ -0,0 +1,20 @@
+// Title: Validate Binary Search Tree
+            // Difficulty: Medium
+            // Language: C++
+            // Link: https://leetcode.com/problems/validate-binary-search-tree/
+
+// long long minRST(TreeNode* root){
+//     long long mn = LLONG_MAX;
+//     if(root==NULL) return mn;
+maxLST(root->right)));
+// }
+// // max find function
+//     if(root==NULL) return mx;
+//     return max((long long)(root->val),max(maxLST(root->left),
+// long long maxLST(TreeNode* root){
+//     long long mx = LLONG_MIN;
+// // min find function
+
+// // brute force solution for this problem
+// public:
+// class Solution {
