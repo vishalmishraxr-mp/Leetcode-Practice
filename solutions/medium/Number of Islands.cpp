7a0aa754dfa3e5798d8a321443998a22f4e33f76
vishalmishraxr-mp <vishalmishraxr@gmail.com>
2026-09-18 22:32:23 +0530

    Add solution for Number of Islands

---
+++ solutions/medium/Number of Islands.cpp
@@ -0,0 +1,22 @@
+// Title: Number of Islands
+            // Difficulty: Medium
+            // Language: C++
+            // Link: https://leetcode.com/problems/number-of-islands/
+
+                    auto curr = q.front();
+                grid[i][j] = '0';
+                q.push({i,j});
+                while(!q.empty()){
+            for(int j=0;j<cols;j++){
+                if(grid[i][j]=='0') continue;
+
+                cc++;
+
+        int cc = 0;
+        queue<pair<int,int>> q;
+        for(int i=0;i<rows;i++){
+    int numIslands(vector<vector<char>>& grid) {
+        int rows = grid.size();
+        int cols = grid[0].size();
+class Solution {
+public:
