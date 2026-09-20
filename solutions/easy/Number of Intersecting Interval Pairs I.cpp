1ee2b09b8caa8047af62431060be90a36544ba94
vishalmishraxr-mp <vishalmishraxr@gmail.com>
2026-09-20 08:10:21 +0530

    Add solution for Number of Intersecting Interval Pairs I

---
+++ solutions/easy/Number of Intersecting Interval Pairs I.cpp
@@ -0,0 +1,5 @@
+// Title: Number of Intersecting Interval Pairs I
+            // Difficulty: Easy
+            // Language: C++
+            // Link: https://leetcode.com/problems/number-of-intersecting-interval-pairs-i/
+
