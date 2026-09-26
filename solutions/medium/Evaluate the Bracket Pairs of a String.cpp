c51bff4677e84eba00cf58d8fc5bdee698e542a3
vishalmishraxr-mp <vishalmishraxr@gmail.com>
2026-09-26 05:57:12 +0530

    Add solution for Evaluate the Bracket Pairs of a String

---
+++ solutions/medium/Evaluate the Bracket Pairs of a String.cpp
@@ -0,0 +1,20 @@
+// Title: Evaluate the Bracket Pairs of a String
+            // Difficulty: Medium
+            // Language: C++
+            // Link: https://leetcode.com/problems/evaluate-the-bracket-pairs-of-a-string/
+
+            if (s[i] == '(') {
+
+        for (int i = 0; i < s.length(); i++) {
+
+        string ans = "";
+
+        }
+            mp[x[0]] = x[1];
+        for (auto &x : knowledge) {
+
+        unordered_map<string, string> mp;
+        
+    string evaluate(string s, vector<vector<string>>& knowledge) {
+public:
+class Solution {
